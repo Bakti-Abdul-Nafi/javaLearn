cbcc03801b38ca2fe6db760da14550a282ef894c
bakti <abdulbakti28@gmail.com>
2022-11-22 13:10:43 +0700

    max number in array

--- src/traCatch.java
+++ src/traCatch.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class traCatch {
     public static void main(String args[]){
-        System.out.println("Hublalala");
+        Scanner input = new Scanner(System.in);
+        System.out.println("##Program java nilai terbesar##");
+        System.out.println("===============================");
+        System.out.println();
+
+        int[] arr = new int[100];
+        int arr_count,i,max_num;
+        System.out.println("Input jumlah elemen pada array:");
+        arr_count = input.nextInt();
+        System.out.println("Input " + arr_count + " angka :");
+
+        //simpan angka yang dimasukan kedalam array
+        for(i = 0; i< arr_count; i++){
+            arr[i] = input.nextInt();
+        }
+        System.out.println();
+
+        //mencari nilai terbesar
+        max_num = arr[0];
+        for (i = 0; i< arr_count; i++){
+            if(arr[i] > max_num){
+                max_num = arr[i];
+            }
+        }
+        System.out.println("Nilai array terbesar yaitu " + max_num);
     }
 }
